40dcaeaa6907a85008ed83128fcc03d45ab7d961
Artem Borovik <polzovatellllk@gmail.com>
2024-09-29 00:02:53 +0300

    ci for web and android

--- web/host.sh
+++ web/host.sh
@@ -1,9 +1,7 @@
+cd $(dirname $0)
 set -ex
 
-# make -C ../examples
+sh build.sh
 
-emcmake cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
-cmake --build build/ --target webrogue -j
-cp build/webrogue.wasm build/webrogue.js root
 cd root
 python3 -m http.server
